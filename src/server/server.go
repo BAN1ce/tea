9172823e11ac82b5c56472b1aa6157ffcb032f79
yangsh <yangsh@senthink.com>
2020-04-10 16:48:54 +0800

    feat:server 添加心跳包的设置，超时时间设置，创建客户端监听客户端

--- src/server/server.go
+++ src/server/server.go
@@ -1,26 +1,39 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"github.com/google/uuid"
 	"net"
 	"sync"
+	"tea/src/client"
+	"tea/src/unpack"
+	"time"
 )
 
 type Server struct {
-	name       string
-	port       int
-	isStop     bool
-	clients    sync.Map
-	clientDone chan uuid.UUID
-	listener   net.Listener
-	mutex      *sync.RWMutex
+	name          string
+	port          int
+	isStop        bool
+	clients       sync.Map
+	clientDone    chan uuid.UUID
+	listener      net.Listener
+	protocol      unpack.Protocol
+	mutex         *sync.RWMutex
+	onServerStart func(s *Server)
+	onConnect     client.OnConnect
+	onMessage     client.OnMessage
+	onClose       client.OnClose
+	hbContent     []byte
+	hbInterval    time.Duration
+	hbTimeout     time.Duration
 }
 
 func NewServer(addr net.Addr) *Server {
 
 	var err error
 	server := new(Server)
+	server.isStop = true
 	server.listener, err = net.Listen(addr.Network(), addr.String())
 
 	server.clientDone = make(chan uuid.UUID, 1000)
@@ -33,22 +46,40 @@ func NewServer(addr net.Addr) *Server {
 	return server
 }
 
-func (s *Server) Run() {
+func (s *Server) SetOnConnect(onConnect client.OnConnect) {
+	s.onConnect = onConnect
+}
+
+func (s *Server) SetOnMessage(onMessage client.OnMessage) {
+	s.onMessage = onMessage
+}
+
+func (s *Server) SetOnClose(onClose client.OnClose) {
+	s.onClose = onClose
+}
+func (s *Server) SetProtocol(protocol unpack.Protocol) {
+	s.protocol = protocol
+}
+func (s *Server) Run(ctx context.Context) {
 
 	s.mutex.Lock()
 
 	if s.isStop == false {
 		fmt.Println("Server is Running")
 	} else {
+		if s.onServerStart != nil {
+			s.onServerStart(s)
+		}
 		s.isStop = true
+		if s.protocol == nil {
+			panic("Set protocol first please")
+		}
 	}
 	s.mutex.Unlock()
 
 	s.mutex.RLock()
 
 	if s.isStop == true {
-
-		//todo server 启动
 		go func() {
 			for {
 				select {
@@ -59,14 +90,41 @@ func (s *Server) Run() {
 			}
 		}()
 
+		go func() {
+			for {
+				conn, err := s.listener.Accept()
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				go s.acceptHandle(ctx, conn)
+
+			}
+		}()
+
 	}
 
 	s.mutex.RUnlock()
 
 }
 
-func (s *Server) acceptHandle(conn *net.Conn) {
+func (s *Server) acceptHandle(ctx context.Context, conn net.Conn) {
 
-	//todo onConnect 事件
-	
+	uid := uuid.New()
+
+	c := client.NewClient(conn, uid, s.clientDone, s.protocol, s.onMessage, s.onConnect, s.onClose)
+
+	s.clients.Store(uid, conn)
+
+	if s.hbInterval > 0 && s.hbContent != nil {
+		c.SetHbContent(s.hbContent)
+		c.SetHbInterval(s.hbInterval)
+	}
+
+	if s.hbTimeout > 0 {
+		c.SetHbTimeout(s.hbTimeout)
+	}
+
+	c.Run(ctx)
+
 }
