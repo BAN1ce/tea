f8c6b66cd463533f6c9f1b9e7c888c6b1d37d395
yangsh <yangsh@senthink.com>
2020-04-13 17:23:11 +0800

    包引入循环错误

--- example/main.go
+++ example/main.go
@@ -2,43 +2,41 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 	"tea/src/client"
+	"tea/src/mqtt"
 	"tea/src/server"
-	"time"
+	"tea/src/utils"
 )
 
 func main() {
 
-	netAddr2, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:8001")
+	mqtt.Boot()
+	netAddr2, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:1883")
 	s2 := server.NewServer(netAddr2)
 
 	s2.SetProtocol(func() bufio.SplitFunc {
-		return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-			if atEOF {
-				return len(data), data[:len(data)], errors.New("EOF")
-			}
-			index := bytes.IndexAny(data, "&")
-			if index != -1 {
-				return index + 1, data[:index], nil
-			}
-			return 0, nil, nil
-		}
+
+		return mqtt.Input()
+
 	})
 	s2.SetOnMessage(func(msg []byte, client *client.Client) error {
-		client.Write(msg)
+
+		pack := mqtt.Decode(msg)
+
+		mqtt.HandleCmd(*pack,client)
+
+		for _, v := range msg {
+			fmt.Printf("%08s ", utils.ConvertToBin(int(v)))
+		}
 		return nil
 	})
 
-	s2.SetHbInterval(20 * time.Second)
-	s2.SetHbContent([]byte{'&'})
 	s2.Run(context.Background())
 
 	c := make(chan os.Signal)
