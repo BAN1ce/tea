5113dea18cf6a58d3a46bbc04cf64b77d1fbe13d
yangsh <yangsh@senthink.com>
2020-04-14 17:14:20 +0800

    添加订阅和发布两条命令的路由

--- src/mqtt/route.go
+++ src/mqtt/route.go
@@ -3,6 +3,7 @@ package mqtt
 import (
 	"tea/src/client"
 	"tea/src/mqtt/handle"
+	"tea/src/mqtt/protocol"
 )
 
 var Route []handle.Handle
@@ -10,15 +11,21 @@ var Route []handle.Handle
 func Boot() {
 	Route = make([]handle.Handle, 17)
 	Route[1] = handle.NewConnect()
+	Route[3] = handle.NewPublish()
 
 }
 
-func HandleCmd(pack Pack, client *client.Client) {
+func HandleCmd(pack protocol.Pack, client *client.Client) {
 
 	var getHandle handle.Handle
 	switch pack.Cmd {
 	case 1:
 		getHandle = handle.NewConnect()
+	case 3:
+		getHandle = handle.NewPublish()
+
+	case 8:
+		getHandle = handle.NewSubscribe()
 
 	}
 
