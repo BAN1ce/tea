d0f8f622d273bbe3c1e4165d13238c6061ce4434
BAN1ce <suhangyang@gmail.com>
2020-05-13 18:05:34 +0800

    gossip协议的对接

--- src/distributed/delegate.go
+++ src/distributed/delegate.go
@@ -1,6 +1,7 @@
 package distributed
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/hashicorp/memberlist"
 )
@@ -11,30 +12,61 @@ type delegate struct {
 type eventDelegate struct {
 }
 
+type broadcast struct {
+	msg    []byte
+	notify chan<- struct{}
+}
+
+func (b *broadcast) Invalidates(other memberlist.Broadcast) bool {
+	return false
+}
+
+func (b *broadcast) Message() []byte {
+	return b.msg
+}
+
+func (b *broadcast) Finished() {
+	if b.notify != nil {
+		close(b.notify)
+	}
+}
+
 func (d delegate) NodeMeta(limit int) []byte {
 
 	return nil
 }
 
-func (d delegate) NotifyMsg([]byte) {
+func (d delegate) NotifyMsg(msg []byte) {
+	fmt.Println("NotifyMsg", msg)
+
+	update := newUpdate()
+	if err := json.Unmarshal(msg, update); err != nil {
+
+		fmt.Println(update)
+		handle(update)
+	}
 	return
 }
 
 func (d delegate) GetBroadcasts(overhead, limit int) [][]byte {
-	return nil
+	return broadcasts.GetBroadcasts(overhead, limit)
 }
 
 func (d delegate) LocalState(join bool) []byte {
 
-	return  nil
+	return nil
 }
 
 func (d delegate) MergeRemoteState(buf []byte, join bool) {
+
+	fmt.Println("MergeRemoteState", buf)
 	return
 }
 
 func (e eventDelegate) NotifyJoin(n *memberlist.Node) {
 	//fixme
+
+	fmt.Println("somebody join", n.Name)
 	MemberJoin(n)
 }
 
